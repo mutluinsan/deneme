30c0f810dd93541be7d683fac8feebcc56ae9803
Mutlu İnsan <mutluinsan1445@gmail.com>
2023-12-10 22:11:54 +0200

    ödev 1

---
+++ backend/models/book.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    publishYear: {
+        type: Number
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Book', bookSchema);
+
