de895fef895de5289d6c4ff5b322f545de4e2970
Mutlu İnsan <mutluinsan1445@gmail.com>
2023-12-20 20:45:09 +0200

    sonunda bitti çok şükür

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+// localhost:0404
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const connect = require('./db/connect_mongoose')
 const books = require('./routes/books')
+const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 require('dotenv').config()
 
 const port = process.env.PORT || 3000
@@ -9,12 +13,14 @@ const port = process.env.PORT || 3000
 // middleware
 app.use(express.json())
 app.use('/api/v1/books', books)
+app.use(notFound)
+app.use(errorHandler)
 
 const start = async () => {
     try {
         await connect(process.env.MONGO_URL);
         app.listen(port, () => {
-            console.log("sunucu calismaya basladi....." + port);
+            console.log("sunucu çalışmaya başladı....." + port);
         })
     } catch (error) {
         console.log(error);
